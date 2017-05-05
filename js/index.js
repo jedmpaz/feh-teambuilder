608d88b4a1ed0238dd138cd69e1567aaa4e83e02
jedmpaz <jedmpaz@gmail.com>
2017-05-05 11:23:45 -0400

    change y-axis by stat, filter units on graph by color, axes formatted

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.onload = start;
 
+/* global arrays for csvs */
 var raw_data = [];
 var units = {};
 var weapons = {};
@@ -11,8 +12,11 @@ var skillsC = {};
 var skillsS = {};
 var names = [];
 
+var colors = {Red: "#a52512", Blue: "#1c2777", Green: "#0e7a2d", Colorless: "gray", Stat: "#d8d165"};
+
 var tip;
 
+/* scatter plot div sizes */
 var s_width = 600,
         s_height = 600,
         s_padding = 50;
@@ -22,9 +26,18 @@ var margin = {
         bottom: 40,
         left: 100
     };
-
 var x, y, xAxis, yAxis;
 
+/* keep track of which button is pressed */
+var hpClicked = false;
+var atkClicked = true;
+var spdClicked = false;
+var defClicked = false;
+var resClicked = false;
+var redClicked = true;
+var blueClicked = true;
+var greenClicked = true;
+var colorlessClicked = true;
 
 function start() {
     d3.select("#scatter")
@@ -97,13 +110,19 @@ function start() {
                 return +d.ATK;
             })+1]);
 
+//        y.domain([10,62]);
+
         scatterplot.selectAll(".dot").data(data)
             .enter().append("circle")
             .attr("class", "dot")
-            .style("fill", "#d8d165")
+            .style("fill", function (d) {
+                if (d.Color == "Red") {return colors[d.Color]};
+                if (d.Color == "Blue") {return colors[d.Color]};
+                if (d.Color == "Green") {return colors[d.Color]};
+                if (d.Color == "Colorless") {return colors[d.Color]};
+            })
             .style("stroke", "#969696")
             .attr("cx", function (d) {
-                console.log(x(+d.Total) + "," + y(+d.ATK))
                 return x(+d.Total);
             })
             .attr("cy", function (d) {
@@ -121,12 +140,7 @@ function start() {
                     .transition()
                     .duration(300)
                     .attr("r", 10)
-                    .style("fill", function (d) {
-                        if (d.Color == "Red") {return "#a52512"};
-                        if (d.Color == "Blue") {return "#1c2777"};
-                        if (d.Color == "Green") {return "#0e7a2d"};
-                        if (d.Color == "Colorless") {return "gray"};
-                    })
+                    .style("stroke", "#d8d165")
             })
             .on("mouseout", function (d) {
                 tip.transition()
@@ -136,22 +150,24 @@ function start() {
                          .transition()
                          .duration(300)
                          .attr("r", 5)
-                         .style("fill", "#d8d165");
+                         .style("stroke", "969696");
             });
 
         scatterplot.append("g")
             .attr("transform", "translate(" + s_padding / 2 + ",0)")
-            .attr("class", "y-axis")
+            .attr("class", "axis")
+            .attr("id", "y-axis")
             .call(yAxis)
             .append("text")
             .attr("transform", "rotate(-90)")
             .attr("x", 0 - (s_height / 2))
             .attr("y", 0 - margin.left)
             .style("text-anchor", "middle")
-            .text("Danceability");
+            .text("stat");
 
         scatterplot.append("g")
             .attr("transform", "translate(0," + (s_height - s_padding) + ")")
+            .attr("class", "axis")
             .call(xAxis)
             .selectAll("text")
             .style("text-anchor", "end")
@@ -161,8 +177,189 @@ function start() {
                 return "rotate(-65)"
             })
 
+
+        /* update scatter plot y-axis and y-value for selected stat */
+        d3.selectAll(".stat-button").on("click", function() {
+            resetStatButtons();
+
+            var stat;
+
+            if(d3.select(this).attr('id') == "hpButton") {
+                hpClicked = true;
+                stat = "HP";
+            } else if(d3.select(this).attr('id') == "atkButton") {
+                atkClicked = true;
+                stat = "ATK";
+            } else if(d3.select(this).attr('id') == "spdButton") {
+                spdClicked = true;
+                stat = "SPD";
+            } else if(d3.select(this).attr('id') == "defButton") {
+                spdClicked = true;
+                stat = "DEF";
+            } else if(d3.select(this).attr('id') == "resButton") {
+                spdClicked = true;
+                stat = "RES";
+            }
+
+            d3.select(this)
+                .transition()
+                .delay(function (d, i) {
+                    return i * 100;
+                })
+                .style("background-color", "steelblue");
+
+            y.domain([
+                d3.min(data, function(d) {
+                    return +d[stat];
+                })-1,
+                d3.max(data, function(d) {
+                    return +d[stat];
+                })+1]);
+
+            d3.select("#y-axis")
+                .transition()
+                .duration(750)
+                .call(yAxis);
+
+            d3.selectAll(".dot").data(data)
+                .transition()
+                .duration(750)
+                .attr("cy", function (d) {
+                    return y(+d[stat]);
+                })
+        })
+
+        d3.selectAll(".color-button").on("click", function() {
+            var stat;
+
+            if(d3.select(this).attr('id') == "redButton") {
+                redClicked = !redClicked;
+            } else if(d3.select(this).attr('id') == "blueButton") {
+                blueClicked = !blueClicked;
+            } else if(d3.select(this).attr('id') == "greenButton") {
+                greenClicked = !greenClicked;
+            } else if(d3.select(this).attr('id') == "colorlessButton") {
+                colorlessClicked = !colorlessClicked;
+            }
+
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .style("background-color", function() {
+                    if(d3.select(this).attr('id') == "redButton") {
+                        if(redClicked) {
+                            return colors.Red;
+                        } else {
+                            return "white";
+                        }
+                    } else if(d3.select(this).attr('id') == "blueButton") {
+                        if(blueClicked) {
+                            return colors.Blue;
+                        } else {
+                            console.log("white");
+                            return "white";
+                        }
+                    } else if(d3.select(this).attr('id') == "greenButton") {
+                        if(greenClicked) {
+                            return colors.Green;
+                        } else {
+                            return "white";
+                        }
+                    } else if(d3.select(this).attr('id') == "colorlessButton") {
+                        if(colorlessClicked) {
+                            return colors.Colorless;
+                        } else {
+                            return "white";
+                        }
+                    }
+                })
+                .style("color", function() {
+                    if(d3.select(this).attr('id') == "redButton") {
+                        if(redClicked) {
+                            return "white";
+                        } else {
+                            return "black";
+                        }
+                    } else if(d3.select(this).attr('id') == "blueButton") {
+                        if(blueClicked) {
+                            return "white";
+                        } else {
+                            return "black";
+                        }
+                    } else if(d3.select(this).attr('id') == "greenButton") {
+                        if(greenClicked) {
+                            return "white";
+                        } else {
+                            return "black";
+                        }
+                    } else if(d3.select(this).attr('id') == "colorlessButton") {
+                        if(colorlessClicked) {
+                          return "white";
+                        } else {
+                          return "black";
+                        }
+                    }
+                })
+                .style("border-style", function() {
+                    if(d3.select(this).attr('id') == "redButton") {
+                        if(redClicked) {
+                            return "none";
+                        } else {
+                            return "solid";
+                        }
+                    } else if(d3.select(this).attr('id') == "blueButton") {
+                        if(blueClicked) {
+                            return "none";
+                        } else {
+                            return "solid";
+                        }
+                    } else if(d3.select(this).attr('id') == "greenButton") {
+                        if(greenClicked) {
+                          return "none";
+                        } else {
+                          return "solid";
+                        }
+                    } else if(d3.select(this).attr('id') == "colorlessButton") {
+                        if(colorlessClicked) {
+                          return "none";
+                        } else {
+                          return "solid";
+                        }
+                    }
+                });
+
+            d3.selectAll(".dot")
+                .transition()
+                .duration(200)
+                .style("display", function(d) {
+                    if(!redClicked && d.Color == "Red") {
+                        return "none";
+                    }
+                    if(!blueClicked && d.Color == "Blue") {
+                        return "none";
+                    }
+                    if(!greenClicked && d.Color == "Green") {
+                        return "none";
+                    }
+                    if(!colorlessClicked && d.Color == "Colorless") {
+                        return "none";
+                    }
+                 });
+        })
+
         console.log(units);
     });
+}
+
+/* Stat, Color Buttons Functions */
+
+function resetStatButtons() {
+    hpClicked = false;
+    atkClicked = false;
+    spdClicked = false;
+    defClicked = false;
+    resClicked = false;
+    d3.selectAll(".stat-button").style("background-color", colors.Stat);
 }
 
 /* Search & results functions */
@@ -182,76 +379,74 @@ function heroSearch() {
     var newElement =
     "<div class='hero-search-table' id='"+hero+"'>"
         +"<h3>"+hero+"</h3>"
-            +"<div>"
-                +"<table class='stats1'>"
-                    +"<thead>"
-                        +"<tr>"
-                            +"<th>Skill</th>"
-                            +"<th>Name</th>"
-                        +"</tr>"
-                    +"</thead>"
-                    +"<tbody>"
-                        +"<tr>"
-                            +"<td>Weapon</td>"
-                            +"<td>"+units[hero].Weapon+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>Assist</td>"
-                            +"<td>"+units[hero].Assist+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>Special</td>"
-                            +"<td>"+units[hero].Special+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>A Slot</td>"
-                            +"<td>"+units[hero].SkillA+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>B Slot</td>"
-                            +"<td>"+units[hero].SkillB+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>C Slot</td>"
-                            +"<td>"+units[hero].SkillC+"</td>"
-                        +"</tr>"
-                    +"</tbody>"
-                +"</table>"
-                +"<table class='stats2'>"
-                    +"<thead>"
-                        +"<tr>"
-                            +"<th>Stat</th>"
-                            +"<th>Value</th>"
-                        +"</tr>"
-                    +"</thead>"
-                    +"<tbody>"
-                        +"<tr>"
-                            +"<td>HP</td>"
-                            +"<td>"+heroStats.HP+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>ATK</td>"
-                            +"<td>"+heroStats.ATK+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>SPD</td>"
-                            +"<td>"+heroStats.SPD+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>DEF</td>"
-                            +"<td>"+heroStats.DEF+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>RES</td>"
-                            +"<td>"+heroStats.RES+"</td>"
-                        +"</tr>"
-                        +"<tr>"
-                            +"<td>Total</td>"
-                            +"<td>"+units[hero].Total+"</td>"
-                        +"</tr>"
-                    +"</tbody>"
-                +"</table>"
-            +"</div>"
+            +"<table class='stats1'>"
+                +"<thead>"
+                    +"<tr>"
+                        +"<th>Skill</th>"
+                        +"<th>Name</th>"
+                    +"</tr>"
+                +"</thead>"
+                +"<tbody>"
+                    +"<tr>"
+                        +"<td>Weapon</td>"
+                        +"<td>"+units[hero].Weapon+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>Assist</td>"
+                        +"<td>"+units[hero].Assist+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>Special</td>"
+                        +"<td>"+units[hero].Special+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>A Slot</td>"
+                        +"<td>"+units[hero].SkillA+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>B Slot</td>"
+                        +"<td>"+units[hero].SkillB+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>C Slot</td>"
+                        +"<td>"+units[hero].SkillC+"</td>"
+                    +"</tr>"
+                +"</tbody>"
+            +"</table>"
+            +"<table class='stats2'>"
+                +"<thead>"
+                    +"<tr>"
+                        +"<th>Stat</th>"
+                        +"<th>Value</th>"
+                    +"</tr>"
+                +"</thead>"
+                +"<tbody>"
+                    +"<tr>"
+                        +"<td>HP</td>"
+                        +"<td>"+heroStats.HP+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>ATK</td>"
+                        +"<td>"+heroStats.ATK+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>SPD</td>"
+                        +"<td>"+heroStats.SPD+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>DEF</td>"
+                        +"<td>"+heroStats.DEF+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>RES</td>"
+                        +"<td>"+heroStats.RES+"</td>"
+                    +"</tr>"
+                    +"<tr>"
+                        +"<td>Total</td>"
+                        +"<td>"+units[hero].Total+"</td>"
+                    +"</tr>"
+                +"</tbody>"
+            +"</table>"
     +"</div>";
 
     $(newElement).prependTo('.results').hide().slideDown(200);
